6ef3929cd9f2e2ded7e1751204764ad4b2e37d9d
Raul <raulbzga@gmail.com>
2018-07-20 11:52:14 -0400

    search different data

--- dropout_data.py
+++ dropout_data.py
@@ -18,33 +18,45 @@ std = np.std(X_tr, axis = 0)
 sample_vector = np.random.normal(loc = mean, scale = std)
 
 #%%
-
-def ve_selec(X):
+import csv
+def vec_selec(X, i):
     
     mean = np.mean(X, axis = 0)
     std = np.std(X, axis = 0)
     
-    V = random.choice(X)
+    V = X[i]
     
     return V, mean, std
 
 def add_noise(X, N_entries):
-    
-    V, mean, std = vec_selec(X)
-    
-    sample_vector = np.random.normal(loc = mean, scale = std)
-    
-    indexes = np.array(len(V))
-    
-    R_indexes = np.shuffle(indexes)
-    
-    R = R_indexes[:N_entries]
-    
-    
-    for i in R:
+    X_new = []
+    for i in range(len(X)):
+        V, mean, std = vec_selec(X, i)
         
-        V[i] = sample_vector[i]
+        sample_vector = np.random.normal(loc = mean, scale = std)
+        
+        indexes = np.arange(V.shape[0])
+        np.random.shuffle(indexes)
+        
+        R = indexes[:N_entries]
+        
+        
+        for i in R:
+            
+            V[i] = sample_vector[i]
+        X_new.append(V)
+    X_new = np.array(X_new)
     
-    return V
+    return X_new
 
+def grid_search(X):
     
+    X_train_new = np.array
+    for N_entries in range(0, 21):
+    
+        X_tarin_new = add_noise(X, N_entries)
+        name = 'X_train_drop_'+ str(N_entries)
+        P = np.save(X_train_new, (name, N_entries))
+    return P
+        
+        
